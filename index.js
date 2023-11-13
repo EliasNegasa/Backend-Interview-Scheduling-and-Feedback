028564bd479f954482027df6be43a3123fa71f4e
= <=>
2023-11-13 17:30:27 +0300

    404 middleware added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import helmet from 'helmet';
 import dotenv from 'dotenv/config';
 import connectDB from './config/db';
 import router from './src/routes';
+import { errorHandler, notFound } from './src/middlewares/error';
 
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3000;
@@ -20,6 +21,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', router);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log('Server Running on', PORT);
--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,3 +1,9 @@
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
@@ -20,4 +26,4 @@ const errorHandler = (err, req, res, next) => {
   });
 };
 
-export { errorHandler };
+export { notFound, errorHandler };
