183a49c1becfd13fdec8e7e7a86e5dc6f8a174fc
= <=>
2023-11-16 19:05:20 +0300

    schedule routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import userRouter from './user';
 import clientRouter from './client';
+import scheduleRouter from './schedule';
 
 const router = Router();
 
 router.use('/users', userRouter);
 router.use('/clients', clientRouter);
+router.use('/schedules', scheduleRouter);
 
 export default router;
