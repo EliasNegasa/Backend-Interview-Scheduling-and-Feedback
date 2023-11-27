926ed30ca10420a6be9f5bbc9206264b29ee5c77
= <=>
2023-11-27 21:08:42 +0300

    users api update and refactored

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,22 +1,21 @@
 import asyncHandler from 'express-async-handler';
 import User from '../models/user';
 import generateUrl from '../utils/generate-url';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcryptjs';
-const generateToken = (id) => {
-  return jwt.sign({ id }, "secret", {
-    expiresIn: "1d"  });
-};
+
 const generateUserResponse = (user) => {
   return {
     _id: user._id,
     firstName: user.firstName,
+    surName: user.surName,
     lastName: user.lastName,
     email: user.email,
     role: user.role,
     isActive: user.isActive,
+    technicalRecruiter: user.technicalRecruiter,
     createdAt: user.createdAt,
     updatedAt: user.updatedAt,
+    createdBy: user.createdBy,
+    updatedBy: user.updatedBy,
   };
 };
 
@@ -69,51 +68,19 @@ const getUsers = asyncHandler(async (req, res) => {
 });
 
 const createUser = asyncHandler(async (req, res) => {
-  const createdBy = req.user._id; // Assuming the authenticated user's ID is stored in req.user._id
-  const userData = { ...req.body, createdBy }; // Merge createdBy field into the request body
-  const user = await User.create(userData);
+  req.body.createdBy = req.user._id;
 
-  res.status(201).json({
-    message:'User Successfully Registered',
-    _id:user._id,
-    name:user.firstName,
-    email:user.email,
-    token:generateToken(user._id)
-})        
+  const user = await User.create(req.body);
+
+  res.status(201).json(generateUserResponse(user));
 });
-const  LoginUser=asyncHandler(async(req,res)=>{
-  const {email,password}=req.body;
-  try {
-      if(!email||!password){
-          return   res.status(400).json({msg:'please fill out all form'})
-      }
-      const userExists = await User.findOne({email});
-      if(userExists){
-      const pass = await bcrypt.compare(password,userExists.password)
-
-
-      if(userExists&&pass){
-          res.status(200).json({
-              message:'User Successfully logged',
-              _id:userExists._id,
-              name:userExists.name,
-              email:userExists.email,
-              token:generateToken(userExists._id)
-          })
-      }        
-  else{
-          return   res.status(400).json({msg:'invaid user credintial'})
-      }   } 
-  } catch (error) {
-      console.log(error.message)
-      res.status(500).json({message:error.message})
-  }
-})
 
 const updateUser = asyncHandler(async (req, res) => {
   const { id } = req.params;
+  const { _id: userId } = req.user;
 
   const { password } = req.body;
+  req.body.updatedBy = userId;
 
   const user = await User.findById(id);
 
@@ -122,14 +89,16 @@ const updateUser = asyncHandler(async (req, res) => {
     throw new Error('User not Found');
   }
 
+  if (userId.toString() !== user.createdBy.toString()) {
+    res.status(403);
+    throw new Error('Not Authorized to update this user');
+  }
+
   if (password) {
     req.body.password = await user.updatePassword(password);
   }
-  const updatedBy = req.user._id; // Assuming the authenticated user's ID is stored in req.user._id
-  const userData = { ...req.body, updatedBy }; // Merge createdBy field into the request body
 
-
-  const updatedUser = await User.findByIdAndUpdate(id, userData, {
+  const updatedUser = await User.findByIdAndUpdate(id, req.body, {
     new: true,
     runValidators: true,
   });
@@ -139,8 +108,15 @@ const updateUser = asyncHandler(async (req, res) => {
 
 const deleteUser = asyncHandler(async (req, res) => {
   const { id } = req.params;
-  const updatedBy = req.user._id;
-  const user = await User.findByIdAndUpdate(id, { isActive: false ,updatedBy});
+  const { _id: userId } = req.user;
+  req.body.updatedBy = userId;
+
+  if (userId.toString() === id.toString()) {
+    res.status(400);
+    throw new Error('You cannot delete your own account');
+  }
+
+  const user = await User.findByIdAndUpdate(id, { isActive: false });
 
   if (!user) {
     res.status(404);
@@ -150,4 +126,4 @@ const deleteUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'User Deleted' });
 });
 
-export { getUserById, getUsers, createUser, updateUser, deleteUser, LoginUser };
+export { getUserById, getUsers, createUser, updateUser, deleteUser };
